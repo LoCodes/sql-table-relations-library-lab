b653460151540fe84ca93457292084ece551034c
Loren Cipriano <loren25cipriano@gmail.com>
2021-01-17 23:27:16 -0800

    commit tree 91bfe50c96ff06fffdc77a2efe74f78f49c77bc4

---
+++ lib/update.sql
@@ -0,0 +1,11 @@
+-- updating
+--   updates the species of the last character in the characters table to "Martian"
+
+UPDATE characters 
+SET species = 'Martian'
+WHERE id = 8;
+
+-- querying
+--   selects all of the books titles and years in the 
+--   first series and orders them chronologically 
+
